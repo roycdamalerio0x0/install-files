d6977315c43c148e78d7bb2e741813062e5df597
roycdamalerio0x0 <roycdamalerio.0x0@gmail.com>
2024-05-12 18:10:28 +0800

    added install files for new system

---
+++ deb_essentials.sh
@@ -0,0 +1,13 @@
+# Update and Upgrade
+sudo apt update && sudo update upgrade
+
+# Install essential tools
+sudo apt install vim tmux pulsuaudio alsa-utils pavucontrol firefox code git dconf-editor
+
+# Download fonts
+mkdir ~/.fonts
+cd ~/.fonts
+curl -LO https://github.com/roycdamalerio0x0/fonts/archive/refs/heads/main.zip
+fc-cache -vf
+
+
